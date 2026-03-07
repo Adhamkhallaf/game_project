9e83184fd1c81cfcfa6c4a5512a9aafff63cdb45
M-T-E-G <mt378153@gmail.com>
2026-03-08 01:02:18 +0200

    added walking aimation

--- assets/KayKit_Adventurers_2.0_FREE/Characters/gltf/knight.gd
+++ assets/KayKit_Adventurers_2.0_FREE/Characters/gltf/knight.gd
@@ -7,20 +7,20 @@ extends CharacterBody3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-# Matches your screenshot names exactly
 @onready var cam_pivot = $Node3D 
 @onready var spring_arm = $Node3D/SpringArm3D
 
+# 1. NEW: Get reference to the AnimationPlayer
+# Check your scene tree; if it's inside another node, adjust the path (e.g. $Model/AnimationPlayer)
+@onready var anim_player = $AnimationPlayer 
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	# Prevents the camera from jittering when hitting the knight
 	spring_arm.add_excluded_object(get_rid())
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		# Horizontal rotation (Body)
 		rotate_y(-event.relative.x * mouse_sensitivity)
-		# Vertical rotation (Pivot)
 		cam_pivot.rotate_x(-event.relative.y * mouse_sensitivity)
 		cam_pivot.rotation.x = clamp(cam_pivot.rotation.x, deg_to_rad(-70), deg_to_rad(70))
 
@@ -31,7 +31,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Get movement using the actions we just created
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
@@ -44,5 +43,23 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+	# 2. NEW: Animation Logic
+	if is_on_floor():
+		# Use input_dir instead of velocity.length()
+		# This triggers the change the MOMENT you let go of the keys.
+		if input_dir.length() > 0:
+			_play_animation("Rig_Medium_MovementBasic/Walking_A") 
+		else:
+			_play_animation("Rig_Medium_MovementBasic/T-Pose")
+	else:
+		# Optional: Add a jump animation if you have one
+		# _play_animation("Rig_Medium_Movement/Jump_Idle")
+		pass
+
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+# 3. Helper function to prevent the animation from restarting every single frame
+func _play_animation(anim_name: String):
+	if anim_player.current_animation != anim_name:
+		anim_player.play(anim_name)
