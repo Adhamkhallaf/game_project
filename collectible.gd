c9a4fce3c53afe196470958a43f2180818a2a059
M-T-E-G <mt378153@gmail.com>
2026-04-09 17:38:31 +0200

    added goal prompts and collecting logic

--- collectible.gd
+++ collectible.gd
@@ -5,14 +5,14 @@ extends Area3D
 @onready var text_mesh = $Number
 
 func _ready():
-	_update_text()
-	$AnimationPlayer.play("idle")
-
-func _update_text():
 	if text_mesh and text_mesh.mesh:
+		text_mesh.mesh = text_mesh.mesh.duplicate()
 		text_mesh.mesh.text = value
+	$AnimationPlayer.play("idle")
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		print("Character collected: ", value)
+		var manager = get_tree().get_first_node_in_group("GameManager")
+		if manager:
+			manager.add_to_collection(value)
 		queue_free()
--- game_manager.gd
+++ game_manager.gd
@@ -1,33 +1,88 @@
 extends Node3D
 
 @export var bubble_scene: PackedScene 
-@export var current_target_system = "BIN" 
+
+@onready var prompt_label = get_node("../HUD/UI_Root/HBoxContainer/PromptLabel")
+@onready var system_label = get_node("../HUD/UI_Root/HBoxContainer/SystemLabel")
+@onready var collection_label = get_node("../HUD/UI_Root/HBoxContainer/NumberBox/CollectionLabel")
 
 var systems = {
+	"BIN": 2,
+	"OCT": 8,
+	"DEC": 10,
+	"HEX": 16
+}
+
+var system_chars = {
 	"BIN": ["0", "1"],
 	"OCT": ["0", "1", "2", "3", "4", "5", "6", "7"],
 	"DEC": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
 	"HEX": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 }
 
+var source_system: String = ""
+var target_system: String = ""
+var goal_value_decimal: int = 0
+var goal_string_target: String = ""
+var current_collected_string: String = ""
+
 func _ready():
 	randomize()
-	spawn_bubbles(20)
+	start_new_round()
+
+func start_new_round():
+	current_collected_string = ""
+	collection_label.text = ""
+	
+	var system_names = systems.keys()
+	source_system = system_names.pick_random()
+	
+	target_system = source_system
+	while target_system == source_system:
+		target_system = system_names.pick_random()
+	
+	goal_value_decimal = randi_range(1, 31)
+	
+	var prompt_val = _convert_to_base(goal_value_decimal, systems[source_system])
+	goal_string_target = _convert_to_base(goal_value_decimal, systems[target_system])
+	
+	prompt_label.text = prompt_val + " (" + source_system + ") ➔ "
+	system_label.text = "Collect " + target_system + ": "
+	
+	_clear_old_bubbles()
+	spawn_bubbles(25)
+
+func _convert_to_base(decimal_val: int, base: int) -> String:
+	return String.num_int64(decimal_val, base).to_upper()
+
+func _clear_old_bubbles():
+	for child in get_children():
+		if child is Area3D:
+			child.queue_free()
 
 func spawn_bubbles(count: int):
-	if not systems.has(current_target_system):
-		print("Error: System name is incorrect in Inspector!")
-		return
-		
-	var pool = systems[current_target_system]
-	
+	var pool = system_chars[target_system]
 	for i in range(count):
 		var new_bubble = bubble_scene.instantiate()
-		
 		new_bubble.value = pool.pick_random()
-		
+		var rx = randf_range(-12.0, 12.0)
+		var rz = randf_range(-12.0, 12.0)
+		new_bubble.position = Vector3(rx, 1.5, rz)
 		add_child(new_bubble)
-		
-		var rx = randf_range(-15.0, 15.0)
-		var rz = randf_range(-15.0, 15.0)
-		new_bubble.global_position = Vector3(rx, 1.5, rz)
+
+func add_to_collection(val: String):
+	current_collected_string += val
+	collection_label.text = ""
+	for char in current_collected_string:
+		collection_label.text += char + " "
+	
+	_check_win_condition()
+
+func _check_win_condition():
+	if current_collected_string == goal_string_target:
+		print("Correct!")
+		start_new_round()
+	elif not goal_string_target.begins_with(current_collected_string):
+		print("Wrong sequence! Resetting...")
+		current_collected_string = ""
+		collection_label.text = ""
